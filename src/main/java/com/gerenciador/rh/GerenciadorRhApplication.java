8205e7a09321e71f925a01b3e700459d38b1e7d7
Eduardo <edwardviana@gmail.com>
2018-12-24 15:37:46 -0200

    configurando prod profile

--- src/main/java/com/gerenciador/rh/GerenciadorRhApplication.java
+++ src/main/java/com/gerenciador/rh/GerenciadorRhApplication.java
@@ -1,14 +1,18 @@
 package com.gerenciador.rh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GerenciadorRhApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GerenciadorRhApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorRhApplication.class, args);
-
+		logger.info("Iniciando Aplicação");
 	}
 
 }
